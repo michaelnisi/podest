b33b246900a57080487c9a71afa25682ab332567
Michael Nisi <michael.nisi@gmail.com>
2019-04-20 18:17:29 +0200

    Bump version and add group

---
+++ Podest/fav/Recommending.swift
@@ -0,0 +1,13 @@
+//
+//  Recommending.swift
+//  Podest
+//
+//  Created by Michael Nisi on 20.04.19.
+//  Copyright © 2019 Michael Nisi. All rights reserved.
+//
+
+import Foundation
+
+protocol Recommending: SectionedDataSource {
+  
+}
