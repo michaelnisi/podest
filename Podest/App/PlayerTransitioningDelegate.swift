6f07d56cd8d00c7eab71c52c957f85b42c449e33
Michael Nisi <michael.nisi@gmail.com>
2021-04-11 18:59:33 +0200

    Rename

---
+++ Podest/App/PlayerTransitioningDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  PlayerTransitioningDelegate.swift
+//  Podest
+//
+//  Created by Michael Nisi on 24.01.21.
+//  Copyright © 2021 Michael Nisi. All rights reserved.
+//
+
+import UIKit
+
+final class PlayerTransitioningDelegate: NSObject, UIViewControllerTransitioningDelegate {
+  
+  var interactiveDismiss = true
+  
+  init(from presented: UIViewController, to presenting: UIViewController) {
+    super.init()
+  }
+
+  func animationController(
+    forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    nil
+  }
+  
+  func presentationController(
+    forPresented presented: UIViewController,
+    presenting: UIViewController?,
+    source: UIViewController
+  ) -> UIPresentationController? {
+    let controller = PlayerPresentationController(presentedViewController: presented, presenting: presenting)
+    controller.presentedYOffset = UIScreen.main.bounds.height / 3
+    
+    return controller
+  }
+  
+  func interactionControllerForDismissal(
+    using animator: UIViewControllerAnimatedTransitioning
+  ) -> UIViewControllerInteractiveTransitioning? {
+    nil
+  }
+}
